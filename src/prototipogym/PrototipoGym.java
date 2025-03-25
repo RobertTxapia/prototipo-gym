74768b33cf684372a67e717c6e8fb8f7e14b09f6
Robert Tapia <roberttapia613@gmail.com>
2025-03-25 18:16:25 -0400

    Add AuthManager.java and update PrototipoGym.java

--- src/prototipogym/PrototipoGym.java
+++ src/prototipogym/PrototipoGym.java
@@ -1,12 +1,14 @@
 package prototipogym;
 
-/**
- *
- * @author Robert Tapia
- */
+import java.awt.*;
+
 public class PrototipoGym {
     public static void main(String[] args) {
-        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Frame().setVisible(true);
+            }
+        });
     }
     
 }
---
+++ src/prototipogym/util/auth/AuthManager.java
@@ -0,0 +1,41 @@
+package prototipogym.util.auth;
+import prototipogym.model.Usuario;
+
+import java.io.*;
+import java.util.List;
+
+public class AuthManager {
+    private static Usuario usuarioLoquedo;
+
+    public static boolean validarUsuario(String login, String password) {
+        File archivoUsuarios = new File("usuarios.txt");
+
+        try(
+            FileReader fr = new FileReader(archivoUsuarios);
+            BufferedReader br = new BufferedReader(fr);
+
+        ) {
+            String lineas;
+            while ((lineas = br.readLine()) != null) {
+                String[] datos = lineas.split(",");
+                if(datos.length >= 6 && datos[0].equals(login) && datos[1].equals(password)) {
+                    usuarioLoquedo = new Usuario(datos[0], datos[1], Integer.parseInt(datos[2]), datos[3], datos[4], datos[5]);
+                    return true;
+                }
+            }
+        }catch (NumberFormatException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        // to esos catch lo autocompleto el IDE por un error que tenia el FileReader. No me preguntes que hace
+        return false;
+    }
+
+    public static int getNivelAcceso() {
+        // retornar el nivel de acceso del usuario autenticado
+        return (usuarioLoquedo != null) ? usuarioLoquedo.getNivelAcceso() : -1;
+    }
+}
