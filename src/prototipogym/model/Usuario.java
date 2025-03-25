4046053a4ee7debb67caa8ef17112b8b36a7801f
Robert Tapia <roberttapia613@gmail.com>
2025-03-25 17:12:22 -0400

    Update Usuario.java

--- src/prototipogym/model/Usuario.java
+++ src/prototipogym/model/Usuario.java
@@ -16,4 +16,52 @@ public class Usuario {
         this.apellidos = apellidos;
         this.correo = correo;
     }
+    
+    public int getNivelAcceso() {
+        return nivelAcceso;
+    }
+    
+    public void setNivelAcceso(int nivelAcceso) {
+        this.nivelAcceso = nivelAcceso;
+    }
+    
+    public String getLogin() {
+        return login;
+    }
+    
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getApellidos() {
+        return apellidos;
+    }
+    
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+    public String getCorreo() {
+        return correo;
+    }
+    
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
